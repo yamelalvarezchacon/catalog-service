4b46d5583473b51fa2e46dcb1c692ae338ef3a1e
Yamel <yamelalvarezchacon@gmail.com>
2026-01-30 12:35:48 -0500

    Migrate to JDBC, add config, and Docker support

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -1,24 +1,51 @@
 package com.polarbookshop.catalogservice.domain;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
+import java.time.Instant;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
 
 public record Book (
 
-    @NotBlank(message = "The book ISBN must be defined.")
-    @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
-    String isbn,
+        @Id
+        Long id,
 
-    @NotBlank(message = "The book title must be defined.")
-    String title,
+        @NotBlank(message = "The book ISBN must be defined.")
+		@Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
+        String isbn,
 
-    @NotBlank(message = "The book author must be defined.")
-    String author,
+        @NotBlank(message = "The book title must be defined.")
+        String title,
 
-    @NotNull(message = "The book price must be defined.")
-    @Positive(message = "The book price must be greater than zero.")
-    Double price
-    
-){}
+        @NotBlank(message = "The book author must be defined.")
+        String author,
+
+        @NotNull(message = "The book price must be defined.")
+        @Positive(message = "The book price must be greater than zero.")
+        Double price,
+
+        String publisher,
+
+        @CreatedDate
+        Instant createdDate,
+
+        @LastModifiedDate
+        Instant lastModifiedDate,
+
+        @Version
+        int version
+
+){
+
+        public static Book of(String isbn, String title, String author, Double price, String publisher) {
+                return new Book(null, isbn, title, author, price, publisher, null, null, 0);
+        }
+
+}
